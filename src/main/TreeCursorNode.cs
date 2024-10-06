157424ca210ae5ea0862976f73fd6efbe30b1bac
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-10-06 00:29:05 -0600

    work on marshaller stuff

--- src/main/TreeCursorNode.cs
+++ src/main/TreeCursorNode.cs
@@ -1,8 +1,10 @@
-﻿using System.Runtime.InteropServices.Marshalling;
+﻿using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
 
 namespace d9.TreeSitter;
 public readonly struct TreeCursorNode
 {
+    [MarshalAs(UnmanagedType.BStr)]
     public readonly string Type, Name;
     public readonly int StartByte, EndByte;
     public TreeCursorNode(string type, string name, int startByte, int endByte)
